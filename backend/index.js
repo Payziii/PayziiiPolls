a71891c81f277d669cea0e123cfea4be1aab848a
Payziii <velmikhail123@gmail.com>
2026-04-07 14:47:03 +0500

    изменил порт

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const errorHandler  = require('./middleware/errorHandler');
 require('./db');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // ── Middleware ─────────────────────────────────────────────────────────────────
 app.use(cors());
